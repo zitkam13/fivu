c65ab579407380de98fef7f8712e031035fa1132
Karlheinz Zitz <zitkam13@htl-kaindorf.ac.at>
2018-11-09 11:50:20 +0100

    9.11.2018

--- ue02_typescript/dist/main.js
+++ ue02_typescript/dist/main.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("./server");
 class Main {
     static main() {
-        const server = new server_1.Server(4712);
+        const server = new server_1.Server(4711);
         server.start();
     }
 }
--- ue02_typescript/src/server.ts
+++ ue02_typescript/src/server.ts
@@ -1,15 +1,23 @@
 
 // Node.js Modul
-import * as http from 'http';
-import * as express from 'express';
+// import * as http from 'http';
+
+import * as path from 'path';
+
+// Externes Modul
+import * as express from 'express'; // framework für web-server
+// importiere alles (wegen *) unter namen express voom Modul express
 
 export class Server {
 
     private _port: number;
     private _server: express.Express;
-    constructor (port: number) {
+
+    public constructor (port: number) {
         this._port = port;
         this._server = express();
+        this._server.get('/liste', (req, res, next) => this.handleGetListe(req, res, next));
+        this._server.get('/image.png', (req, res, next) => this.sendImage(res));
     }
 
     public start () {
@@ -18,5 +26,15 @@ export class Server {
     }
     public get port () {
         return this._port;
+    }
+    private handleGetListe (req: express.Request, res: express.Response,
+                            next: express.NextFunction) {
+        const filePath = path.join( __dirname, '..', 'assets', 'liste.html');
+        console.log(filePath);
+        res.sendFile(filePath);
+    }
+    private sendImage (res: express.Response) {
+        const filePath = path.join(__dirname, '..', 'assets', 'image.png');
+        res.sendFile(filePath);
     }
 }

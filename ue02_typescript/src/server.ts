612b537068a34855f540b46f889baae0a4942d1c
Karlheinz Zitz <zitkam13@htl-kaindorf.ac.at>
2018-11-23 11:45:36 +0100

    23.11.2018

--- ue02_typescript/src/server.ts
+++ ue02_typescript/src/server.ts
@@ -3,6 +3,7 @@
 // import * as http from 'http';
 
 import * as path from 'path';
+import * as bodyParser from 'body-parser';
 
 // Externes Modul
 import * as express from 'express'; // framework für web-server
@@ -17,9 +18,11 @@ export class Server {
         const assetsPath = path.join(__dirname, '..', 'assets');
         this._port = port;
         this._server = express();
+        this._server.use(bodyParser.json());
+        this._server.use(bodyParser.urlencoded());
         this._server.use('/', express.static(assetsPath));
+        this._server.post('login.html', (req, res, next) => this.handlePostLogin(req, res, next));
         this._server.get('/liste', (req, res, next) => this.handleGetListe(req, res, next));
-        this._server.get('/image.png', (req, res, next) => this.sendImage(res));
     }
 
     public start () {
@@ -29,14 +32,14 @@ export class Server {
     public get port () {
         return this._port;
     }
+    private handlePostLogin(req: express.Request, res: express.Response, next: express.NextFunction) {
+        debugger;
+        next();
+    }
     private handleGetListe (req: express.Request, res: express.Response,
                             next: express.NextFunction) {
         const filePath = path.join( __dirname, '..', 'assets', 'liste.html');
         console.log(filePath);
-        res.sendFile(filePath);
-    }
-    private sendImage (res: express.Response) {
-        const filePath = path.join(__dirname, '..', 'assets', 'image.png');
         res.sendFile(filePath);
     }
 }

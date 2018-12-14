1d22ad16c9cbf1101660b34a38104a0628146608
Karlheinz Zitz <zitkam13@htl-kaindorf.ac.at>
2018-12-14 11:47:41 +0100

    14.12.2018

--- ue03Angular/src/app/app.component.ts
+++ ue03Angular/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  public title = 'ue03Angular';
+  public clock = '??:??:??';
+  public items = [{ myClass: 'red', text: '1'},
+                  { myClass: 'red', text: '2'},
+                  { myClass: 'green', text: '3'}];
+  public myClass = 'green';
+  public constructor () {
+    this.clock = new Date().toLocaleTimeString();
+    setInterval( () => this.handleTimer(), 1000);
+  }
+  private handleTimer() {
+    this.clock = new Date().toLocaleTimeString();
+    // this.items.push('' + (+this.items[this.items.length - 1 ] + 1));
+  }
 }

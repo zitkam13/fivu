613afec5d159a800124cc5f71397dcc00685c02c
Karlheinz Zitz <zitkam13@htl-kaindorf.ac.at>
2019-02-15 11:02:44 +0100

    12.2.2019

--- ue04MyWebsite/src/app/app.module.ts
+++ ue04MyWebsite/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TreesComponent } from './trees.component';
 import { CarsComponent } from './cars.component';
 import { ClockComponent } from './clock.component';
 import { NavbarComponent} from './navbar/navbar.component';
+import { ChartsModule } from 'ng4-charts/ng4-charts';
 import { from } from 'rxjs';
 
 
@@ -20,7 +19,8 @@ import { from } from 'rxjs';
   imports: [
     NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
